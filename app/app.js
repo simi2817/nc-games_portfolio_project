61990f80cb79f856a28844c8a7a07e731c97225b
Simi <simi.sreenivas@gmail.com>
2023-01-16 17:46:44 +0000

    this commit has GET /api/reviews/review_id completed

--- app/app.js
+++ app/app.js
@@ -1,11 +1,36 @@
 const express = require('express');
 
-const { getCategories, getReviews } = require('../controller/controllers');
+const { getCategories, getReviews, getReviewsById } = require('../controller/controllers');
 
 const app = express();
 
 app.get('/api/categories', getCategories);
 
 app.get('/api/reviews', getReviews);
+
+app.get('/api/reviews/:review_id', getReviewsById);
+
+app.use((error, request, response, next) =>
+{
+    if(error.code === '22P02')
+        response.status(400).send({message: 'Bad request!'});
+    else
+        next(error);
+});
+
+app.use((error, request, response, next) =>
+{
+    if(error.status)
+        response.status(error.status).send({message: error.message});
+    else
+        next(error);
+})
+
+
+app.use((error, request, response, next) =>
+{
+        console.error(error);
+        response.status(500).send({message: 'Internal Server Error'});
+})
 
 module.exports = app;
